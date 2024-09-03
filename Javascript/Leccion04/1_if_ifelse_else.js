4a762747c7681f08c9017b66a92f01c3dadb77df
Ron3333 <55641332+Ron3333@users.noreply.github.com>
2024-09-03 10:04:27 -0400

    Leccion 4

---
+++ Javascript/Leccion04/1_if_ifelse_else.js
@@ -0,0 +1,38 @@
+let condicion = false;
+
+//if (condicion) {
+if (3>2) {
+    console.log('condicion verdadera');
+    console.log("fin del programa")
+}
+else{
+    console.log('condicion falsa')
+}
+
+if(3>2)
+    console.log('condicion verdadera');
+else
+    console.log('condicion falsa');
+
+
+
+// Se puede omitir las llaves si solo se tiene una linea de codigo
+
+
+let numero = 4;
+
+if( numero == 1 ){
+    console.log('numero uno');
+}
+else if( numero == 2 ){
+    console.log("numero dos");
+}
+else if( numero == 3){
+    console.log('numero tres');
+}
+else{
+    console.log('numero desconocido');
+}
+
+
+
