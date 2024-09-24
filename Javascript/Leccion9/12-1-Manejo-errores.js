2a93b536fb4b2b9224525860791035906da0306f
Ron3333 <55641332+Ron3333@users.noreply.github.com>
2024-09-24 09:03:17 -0400

    Manejo de Errores

---
+++ Javascript/Leccion9/12-1-Manejo-errores.js
@@ -0,0 +1,16 @@
+
+try{
+	//let x=10; 
+	x=10;
+	//miFuncion();
+	throw "mi error";
+}
+catch(error){
+	console.log(error);
+}
+finally{
+	console.log("termina la revision de errors");
+}
+
+console.log("continuar....");
+
---
+++ Javascript/Leccion9/12-2-throw.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let resultado = "hola"; 
+
+try{
+	//x=10;
+	
+	if(isNaN(resultado)) throw "No es un numero";
+	else if(resultado === "") throw "Cadena vacia";
+	else if(resultado >= 0) throw "valor positivo";
+	else if(resultado < 0) throw "valor negativo";
+}
+catch(error){
+	console.log(error);
+	//console.log(error.name);
+	//console.log(error.message);
+}
+
+console.log("continuar....");
+
